831d7a408f85695e09811c6473c602eee66ddfad
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-12 09:42:40 +0200

    Grouping Operator

--- Assignment1_LinQ/Program.cs
+++ Assignment1_LinQ/Program.cs
@@ -685,7 +685,7 @@ namespace Assignment1_LinQ
 
             #endregion
 
-            #region  Grouping Operators
+            #region  Grouping Operator
             //Use group by to partition a list of numbers by their remainder when divided by 5
             //List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
             //var groupedNumbers = numbers
