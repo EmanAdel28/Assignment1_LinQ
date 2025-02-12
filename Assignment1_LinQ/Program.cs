1adb1c403bbdb8b54ac58ef40a685017abded16c
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-12 06:56:46 +0200

    Restriction Operators Q1

--- Assignment1_LinQ/Program.cs
+++ Assignment1_LinQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using static Assignment1_LinQ.ListGenerator;
+﻿using System.Security.Cryptography;
+using static Assignment1_LinQ.ListGenerator;
 namespace Assignment1_LinQ
 {
     internal class Program
@@ -12,10 +13,25 @@ namespace Assignment1_LinQ
             //    Console.WriteLine(product);
             //}
 
-            foreach (var customer in CustomerList)
+            //foreach (var customer in CustomerList)
+            //{
+            //    Console.WriteLine($"{customer}  \n");
+            //}
+            #endregion
+
+            #region LINQ - Restriction Operators
+            #region Q1
+            //1. Find all products that are out of stock.
+            var prouducts = from P in ProductList
+                            where P.UnitsInStock == 0
+                            select P;
+
+            foreach (var product in prouducts)
             {
-                Console.WriteLine($"{customer}  \n");
+                Console.WriteLine(product);
             }
+
+            #endregion
             #endregion
         }
     }
