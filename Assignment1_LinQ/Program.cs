4023d1037786722b94d804d4158ec715ba9a87ad
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-12 09:08:29 +0200

    Transformation Operators Q6

--- Assignment1_LinQ/Program.cs
+++ Assignment1_LinQ/Program.cs
@@ -462,20 +462,43 @@ namespace Assignment1_LinQ
 
             #endregion
 
-            #region Q4
+            #region Q5
             //5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
-            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
-            int[] numbersB = { 1, 3, 5, 7, 8 };
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
 
-            var pairs = from a in numbersA
-                        from b in numbersB
-                        where a < b
-                        select $"{a} is less than {b}";
+            //var pairs = from a in numbersA
+            //            from b in numbersB
+            //            where a < b
+            //            select $"{a} is less than {b}";
 
-            foreach (var pair in pairs)
+            //foreach (var pair in pairs)
+            //{
+            //    Console.WriteLine(pair);
+            //}
+
+
+
+
+            #endregion
+
+            #region Q5
+            //6.Select all orders where the order total is less than 500.00.
+
+          
+            var orders = CustomerList
+                .SelectMany(c => c.Orders)  
+                .Where(o => o.Total < 500.00M)  
+                .ToList();
+
+            Console.WriteLine("Orders where total is less than 500:");
+            foreach (var order in orders)
             {
-                Console.WriteLine(pair);
+                Console.WriteLine(order);
             }
+
+          
+
 
 
 
