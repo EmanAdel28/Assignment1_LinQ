91dfcd8e730699deee9a7b07b3129a60e52ee17c
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-12 07:02:54 +0200

    Restriction Operators Q2

--- Assignment1_LinQ/Program.cs
+++ Assignment1_LinQ/Program.cs
@@ -22,16 +22,30 @@ namespace Assignment1_LinQ
             #region LINQ - Restriction Operators
             #region Q1
             //1. Find all products that are out of stock.
-            var prouducts = from P in ProductList
-                            where P.UnitsInStock == 0
-                            select P;
+            //var prouducts = from P in ProductList
+            //                where P.UnitsInStock == 0
+            //                select P;
+
+            //foreach (var product in prouducts)
+            //{
+            //    Console.WriteLine(product);
+            //}
+
+            #endregion
+
+            #region Q2
+            //2. Find all products that are in stock and cost more than 3.00 per unit.
+            //var prouducts = from P in ProductList
+            //                where P.UnitsInStock > 0 && P.UnitPrice > 3
+            //                select P;
+
+
+            #endregion
 
             foreach (var product in prouducts)
             {
                 Console.WriteLine(product);
             }
-
-            #endregion
             #endregion
         }
     }
