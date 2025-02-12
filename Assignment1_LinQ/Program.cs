fef2a88a83efb35eac1b92f142ad65cd47419e30
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-12 08:57:23 +0200

    Transformation Operators Q5

--- Assignment1_LinQ/Program.cs
+++ Assignment1_LinQ/Program.cs
@@ -461,6 +461,26 @@ namespace Assignment1_LinQ
 
 
             #endregion
+
+            #region Q4
+            //5. Returns all pairs of numbers from both arrays such that the number from numbersA is less than the number from numbersB.
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            var pairs = from a in numbersA
+                        from b in numbersB
+                        where a < b
+                        select $"{a} is less than {b}";
+
+            foreach (var pair in pairs)
+            {
+                Console.WriteLine(pair);
+            }
+
+
+
+
+            #endregion
             #endregion
 
 
