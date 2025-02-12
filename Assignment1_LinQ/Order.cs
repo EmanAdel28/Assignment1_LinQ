2ba0c299cfc1ed348bf9319885509a6c8ab26952
AmiraAdel28 <amira.adel282001@gmail.com>
2025-02-12 06:51:23 +0200

    Data Setup

---
+++ Assignment1_LinQ/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_LinQ
+{
+    internal class Order
+    {
+
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public decimal Total { get; set; }
+
+        public Order(int orderId, DateTime orderDate, decimal total)
+        {
+            OrderID = orderId;
+            OrderDate = orderDate;
+            Total = total;
+        }
+
+        public Order()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"OrderId: {OrderID} ,OrderDate: {OrderDate.ToShortDateString()} , Total: {Total} ";
+        }
+
+    }
+}
--- Assignment1_LinQ/Program.cs
+++ Assignment1_LinQ/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Assignment1_LinQ
+﻿using static Assignment1_LinQ.ListGenerator;
+namespace Assignment1_LinQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
+            #region Data Setup
+            //foreach (var product in ProductList)
+            //{
+            //    Console.WriteLine(product);
+            //}
+
+            foreach (var customer in CustomerList)
+            {
+                Console.WriteLine($"{customer}  \n");
+            }
+            #endregion
         }
     }
 }
